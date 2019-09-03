79b53bf3cb93cb4184c4120f952b1572ce925561
hatcher <hatcher@debian-haiqunfang.netease>
2019-09-03 17:05:23 +0800

    1. add idea shortcut key 2. update logHandler.py

---
+++ idea/readme.md
@@ -0,0 +1,8 @@
+### 快捷键  
+1. Ctrl+Alt+B -> 跳转到方法实现处     
+2. Ctrl+Alt+left -> 返回至上次浏览的位置   
+3. Alt+left/right -> 切换代码文件   
+4. Ctrl+F4 -> 关闭文件   
+
+### Reference  
+[Intellij IDEA 快捷键整理（TonyCody）](https://www.cnblogs.com/tonycody/p/3257601.html)  
--- logging/logHandler.py
+++ logging/logHandler.py
@@ -3,11 +3,12 @@ import logging
 import logging.handlers
 
 
-file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), '{name}.log'.format(name="logHandler"))
+file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         '{name}.log'.format(name="myapp"))
 file_handler = logging.handlers.TimedRotatingFileHandler(filename=file_name,
                                                          when='S',
-                                                         # interval=1,
-                                                         backupCount=7)
+                                                         interval=1,
+                                                         backupCount=3)
 file_handler.suffix = '%Y%m%d_%H:%M:%S.log'
 formatter = logging.Formatter('[%(asctime)s %(filename)s %(name)s (line:%(lineno)d) %(levelname)s] %(message)s')
 
